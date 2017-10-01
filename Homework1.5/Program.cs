3222d950e830d8a8625986fdd6d4e3486218e7ca
Allison Littman <32345409+AllisonLittman3@users.noreply.github.com>
2017-10-01 18:38:21 -0500

    Simple Dice Game
    For this homework assignment, a Computer and Player took turns rolling 2 6-sided dice. The first to reach 20 after totalling their turns wins!

--- Homework1.5/Program.cs
+++ Homework1.5/Program.cs
@@ -16,9 +16,9 @@ namespace Homework1._5
             Console.WriteLine("Let's Play the Dice Game!");
 
 
-            int i = Round();
-            int j = Round();
-            while (j < 20 && i < 20)
+            int i = Round(); //Player 1
+            int j = Round(); //Computer
+            while (j < 20 && i < 20) //While both Player 1 and Computer have less than 20, the game keeps playing
                 {
 
                     Console.WriteLine("Player 1, roll the dice.");
@@ -37,7 +37,7 @@ namespace Homework1._5
                 i = i + Round();
                 j = j + Round();
             }
-            if (i >= 20)
+            if (i >= 20) //If Player 1 wins
             {
                 Console.WriteLine("Player 1, roll the dice.");
                 Console.Write("Press Enter to roll. ");
@@ -45,7 +45,7 @@ namespace Homework1._5
                 Console.WriteLine("You rolled and your score is " + i + ". Congratulations, you win!");
                 Console.ReadLine();
             }
-            else if (j >= 20)
+            else if (j >= 20) //If the Computer wins
                 {
                 Console.WriteLine("Computer, roll the dice.");
 
@@ -56,12 +56,12 @@ namespace Homework1._5
                 }
             Console.ReadLine();
         }
-        static int RollDice()
+        static int RollDice() //This method rolls 1 6-sided dice
         {
             int rolledNumber = rnd.Next(1, 7);
             return rolledNumber;
 }
-        static int Round()
+        static int Round() //This method is 1 round of 2 rolled 6-sided dice
         {
             int RoundX = RollDice() + RollDice();
             return RoundX;
